52830a11a4b1b9de23c345e8695d9e0888f82ccb
Bevin Duncan <bevin.duncan@gmail.com>
2022-01-29 17:45:22 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1*num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3 = 8
+let num4 = 2
+const mod = num3 / num4;
+
+let max = Math.max(1, 7, 13, 14, 20);
+
